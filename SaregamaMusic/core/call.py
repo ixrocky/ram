36d2b4977f0b9197773c1272aaf2ec32fc92d97f
ixrocky <shivamlkr629@gmail.com>
2026-01-10 15:28:27 +0530

    Remove unused exceptions from call.py

--- SaregamaMusic/core/call.py
+++ SaregamaMusic/core/call.py
@@ -5,17 +5,12 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
+
 from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import (
-    AlreadyJoinedError,
-    NoActiveGroupCall,
-    TelegramServerError,
-)
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityAudio
 from pytgcalls.types.stream import StreamAudioEnded
-
 import config
 from SaregamaMusic import LOGGER, YouTube, app
 from SaregamaMusic.misc import db
