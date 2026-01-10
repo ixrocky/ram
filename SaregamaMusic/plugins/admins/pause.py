734253ddbfde58371b4fbd4e2cc807c0b291e49a
ixrocky <shivamlkr629@gmail.com>
2026-01-10 17:03:44 +0530

    Update pause.py

--- SaregamaMusic/plugins/admins/pause.py
+++ SaregamaMusic/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from SaregamaMusic import app
-from SaregamaMusic.core.call import AMBOTOP
+from SaregamaMusic.core.call import Aviax
 from SaregamaMusic.utils.database import is_music_playing, music_off
 from SaregamaMusic.utils.decorators import AdminRightsCheck
 from SaregamaMusic.utils.inline import close_markup
@@ -15,8 +15,11 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await AMBOTOP.pause_stream(chat_id)
+    try:
+        await SaregamaMusic.pause_stream(chat_id)
+    except Exception:
+        pass
     await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_2"].format(message.from_user.mention),
+        reply_markup=close_markup(_)
     )
-
